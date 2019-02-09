c1ce7ae00f22de615719bfa35bcdae639db19ea2
Bartosz Dąbek <elderaramka@gmail.com>
2019-02-09 14:46:56 +0100

    add auto commit&push script

---
+++ posts/personal-goals/miscellaneous/week_push.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+cd $Repos/bartoszdabek.com/posts/personal-goals/
+git add .
+
+echo -e "${RED}Do you want to push the following changes?${NC}"
+git status
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) git commit -m "week in review"; git push; break;;
+		No ) exit;;
+	esac
+done
